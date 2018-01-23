e3dc33be74d70fc725a45e3c983f2f2ed9ae2a3a
Ivan Chyr <ichyr@MacBook-Pro-Ivan.local>
2018-01-23 23:01:07 +0100

    read all data to array

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ console.log('Input data is ', inputLetterSet)
 // read File
 const readStream = fs.createReadStream('./lib/words.txt', 'utf8');
 let count = 0
+let dictionary = []
 readStream.on('data', function(chunk) {  
-    count += chunk.split('\n').length
+    const data = chunk.split('\n')
+    count += data.length
+    dictionary = dictionary.concat(data)
 }).on('end', function() {
     console.log(count);
+    console.log(dictionary.length)
 });
