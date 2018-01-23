e396d96bc5825ce773eb1fa9172ebf534ebc60c0
Ivan Chyr <ichyr@MacBook-Pro-Ivan.local>
2018-01-23 22:11:21 +0100

    formatted entry point

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // entry point
-
-// read File
 const fs = require('fs')
 
+// read input data
+let inputLetterSet = fs.readFileSync('./input/input.txt', 'utf8')
+inputLetterSet = inputLetterSet.split(' ')
+console.log('Input data is ', inputLetterSet)
+
+
+// read File
 const readStream = fs.createReadStream('./lib/words.txt', 'utf8');
 let count = 0
 readStream.on('data', function(chunk) {  
@@ -10,6 +15,3 @@ readStream.on('data', function(chunk) {
 }).on('end', function() {
     console.log(count);
 });
-
-let inputLetterSet = fs.readFileSync('./input/input.txt', 'utf8')
-inputLetterSet = inputLetterSet.split('\n')
