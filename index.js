243b5fae89c20e7257bdf857abda595f43e007cf
Ivan Chyr <ichyr@MacBook-Pro-Ivan.local>
2018-01-23 22:09:20 +0100

    read dictionary with stream. added file for input data

--- index.js
+++ index.js
@@ -3,5 +3,13 @@
 // read File
 const fs = require('fs')
 
-let wordList = fs.readFileSync('./lib/words.txt');
+const readStream = fs.createReadStream('./lib/words.txt', 'utf8');
+let count = 0
+readStream.on('data', function(chunk) {  
+    count += chunk.split('\n').length
+}).on('end', function() {
+    console.log(count);
+});
 
+let inputLetterSet = fs.readFileSync('./input/input.txt', 'utf8')
+inputLetterSet = inputLetterSet.split('\n')
